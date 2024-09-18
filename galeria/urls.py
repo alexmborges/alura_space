02cddc33b326b54bd7a94afa3aed73b339863b66
Alexandre Borges <alexmborges@gmail.com>
2024-09-18 15:28:41 -0300

    update 3.0

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from galeria.views import index, imagem, buscar
 
-# criando lista para manter todos os endereços relacioados a galera
 urlpatterns = [
     path('', index, name='index'),
     path('imagem/<int:foto_id>', imagem, name='imagem'),
